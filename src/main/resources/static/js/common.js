484680602f3594d55377f83633f57a6a26afed6c
Jeonghee Kim <jeongheekim219@gmail.com>
2022-02-16 00:12:32 +0900

    2022-02-15 Date Selection added.

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -46,3 +46,50 @@ function ajaxTest(){
       });
 
 };
+
+
+function leapYear(date){
+
+    let year = new Date(date).getFullYear();
+    result = false;
+    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0){
+        result = true;
+    }
+    return result;
+}
+
+function lastDay(date){
+
+   var date = new Date(date);
+   var month = new Date(date).getMonth();
+   var lastDayList = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+   var lastDay = lastDayList[month];
+
+   //2월인지 체크
+   if (month == 1){
+        //윤년체크
+        if (leapYear(date)){
+            lastDay = 29;
+        }
+   }
+   return lastDay;
+
+}
+
+function showDate(date){
+
+    var date = new Date(date);
+    var last = lastDay(date);
+
+    var text = "<table><tr>";
+    for (var i = 1; i <= last; i++){
+        text += "<th>" + i + "</th>";
+        if(i % 7 == 0){
+            console.log(i);
+            text += "</tr>";
+        }
+    }
+
+    text += "</table>";
+    return text;
+}
