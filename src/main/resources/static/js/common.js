7b9b053f2a4e338a8dd63aeeb1366ae4e8701fa7
Jeonghee Kim <jeongheekim219@gmail.com>
2022-03-01 16:05:53 +0900

    2022-02-28 Working on date-select dropdown

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -28,35 +28,3 @@ function leapYear(date){
     }
     return result;
 }
-
-function lastDay(date){
-
-   var date = new Date(date);
-   var month = new Date(date).getMonth();
-   var lastDayList = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-   var lastDay = lastDayList[month];
-
-   //2월인지 체크
-   if (month == 1 && (leapYear(date))){
-        lastDay = 29;
-   }
-   return lastDay;
-}
-
-function showDate(date){
-
-    var date = new Date(date);
-    var last = lastDay(date);
-
-    var text = "<table><tr>";
-    for (var i = 1; i <= last; i++){
-        text += "<th>" + i + "</th>";
-        if(i % 7 == 0){
-            console.log(i);
-            text += "</tr>";
-        }
-    }
-
-    text += "</table>";
-    return text;
-}
