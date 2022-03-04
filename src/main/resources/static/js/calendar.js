2e7e4ec1bc5de70b99b44f674c7e0e7e379b855d
allogrooming <h2ad@verh221inLove>
2022-03-04 16:23:34 +0900

    calendar

--- src/main/resources/static/js/calendar.js
+++ src/main/resources/static/js/calendar.js
@@ -35,6 +35,7 @@ function showCalendar(){
         monthCnt++;
         document.getElementById("calendar-body").appendChild($tr);
     }
+    showMain();
 }
 
 function removeCalendar(){
@@ -49,6 +50,8 @@ function removeCalendar(){
 function prev(){
     //inputBox.value = ""; //input-list >> input-wrap으로 변경
     const $divs = document.querySelectorAll('#input-list > div');
+    console.log("$divs");
+    console.log($divs);
     $divs.forEach(function(e){
         e.remove();
     });
@@ -68,19 +71,20 @@ function prev(){
         pageFirst = new Date(first.getFullYear(), first.getMonth()-1, 1);
         first = pageFirst;
     }
+    // 이전으로 클릭 될때 날짜가 같은 달에 대해서 오늘로 보여줌(달력에 크게 띄우는거)
     today = new Date(today.getFullYear(), today.getMonth()-1, today.getDate());
     currentTitle = monthList[first.getMonth()] + '&nbsp;&nbsp;&nbsp;&nbsp;'+ first.getFullYear();
     removeCalendar();
     showCalendar();
-    //showMain();
+    showMain();
     clickedDate1 = document.getElementById(today.getDate());
     clickedDate1.classList.add('active');
-    //clickStart();
-    //reshowingList();
+    clickStart();
+    reshowingList();
 }
 
 function next(){
-    inputBox.value = "";
+    //inputBox.value = "";
     const $divs = document.querySelectorAll('#input-list > div');
     $divs.forEach(function(e){
         e.remove();
@@ -109,5 +113,128 @@ function next(){
     clickedDate1 = document.getElementById(today.getDate());
     clickedDate1.classList.add('active');
     clickStart();
+    reshowingList();
+}
+
+function reshowingList(){
+    keyValue = today.getFullYear() + '' + today.getMonth()+ '' + today.getDate();
+    if(todoList[keyValue] === undefined){
+        inputList.textContent = '';
+        todoList[keyValue] = [];
+        const $divs = document.querySelectorAll('#input-list > div');
+        $divs.forEach(function(e){
+            e.remove();
+        });
+        const $btns = document.querySelectorAll('#input-list > button');
+        $btns.forEach(function(e1){
+            e1.remove();
+        });
+    }else if(todoList[keyValue].length ===0){
+        inputList.textContent = "";
+        const $divs = document.querySelectorAll('#input-list > div');
+        $divs.forEach(function(e){
+            e.remove();
+        });
+        const $btns = document.querySelectorAll('#input-list > button');
+        $btns.forEach(function(e1){
+            e1.remove();
+        });
+    }else{
+        const $divs = document.querySelectorAll('#input-list > div');
+        $divs.forEach(function(e){
+            e.remove();
+        });
+        const $btns = document.querySelectorAll('#input-list > button');
+        $btns.forEach(function(e1){
+            e1.remove();
+        });
+        var $div = document.createElement('div');
+        for(var i = 0; i < todoList[keyValue].length; i++){
+            var $div = document.createElement('div');
+            $div.textContent = '-' + todoList[keyValue][i];
+            var $btn = document.createElement('button');
+            $btn.setAttribute('type', 'button');
+            $btn.setAttribute('id', 'del-ata');
+            $btn.setAttribute('id', dataCnt+keyValue);
+            $btn.setAttribute('class', 'del-data');
+            $btn.textContent = delText;
+            inputList.appendChild($div);
+            inputList.appendChild($btn);
+            $div.addEventListener('click',checkList);
+            $btn.addEventListener('click',deleteTodo);
+            inputBox.value = '';
+            function deleteTodo(){
+                $div.remove();
+                $btn.remove();
+            }
+        }
+    }
+
+}
+var inputBox = document.getElementById('input-box');
+var inputDate = document.getElementById('input-data');
+var inputList = document.getElementById('input-list');
+var delText = 'X';
+//inputDate.addEventListener('click',addTodoList);
+var dataCnt = 1;
+var keyValue = today.getFullYear() + '' + today.getMonth()+ '' + today.getDate();
+let todoList = [];
+todoList[keyValue] = [];
+function addTodoList(){
+    var $div = document.createElement('div');
+    $div.textContent = '-' + inputBox.value;
+    var $btn = document.createElement('button');
+    $btn.setAttribute('type', 'button');
+    $btn.setAttribute('id', 'del-ata');
+    $btn.setAttribute('id', dataCnt+keyValue);
+    $btn.setAttribute('class', "del-data");
+    $btn.textContent = delText;
+    inputList.appendChild($div);
+    inputList.appendChild($btn);
+    todoList[keyValue].push(inputBox.value);
+    dataCnt++;
+    inputBox.value = '';
+    $div.addEventListener('click',checkList);
+    $btn.addEventListener('click',deleteTodo);
+    function deleteTodo(){
+        $div.remove();
+        $btn.remove();
+    }
+}
+console.log(keyValue);
+function checkList(e){
+    e.currentTarget.classList.add('checked');
+}
+
+function showMain(){
+    const mainDay = document.getElementById('main-day');
+    const mainDate = document.getElementById('main-date');
+    mainDay.innerHTML = dayList[today.getDay()];
+    mainDate.innerHTML = today.getDate().toString();
+}
+var clickedDate1 = document.getElementById(today.getDate());
+clickedDate1.classList.add('active');
+var prevBtn = document.getElementById('prev');
+var nextBtn = document.getElementById('next');
+prevBtn.addEventListener('click',prev);
+nextBtn.addEventListener('click',next);
+var tdGroup = [];
+function clickStart(){
+    for(let i = 1; i <= pageYear[first.getMonth()]; i++){
+        tdGroup[i] = document.getElementById(i);
+        tdGroup[i].addEventListener('click',changeToday);
+    }
+}
+function changeToday(e){
+    for(let i = 1; i <= pageYear[first.getMonth()]; i++){
+        if(tdGroup[i].classList.contains('active')){
+            tdGroup[i].classList.remove('active');
+        }
+    }
+    clickedDate1 = e.currentTarget;
+    clickedDate1.classList.add('active');
+    today = new Date(today.getFullYear(), today.getMonth(), clickedDate1.id);
+    showMain();
+    keyValue = today.getFullYear() + '' + today.getMonth()+ '' + today.getDate();
     reshowingList();
 }
