ab8b20fa5a102c3f6ade4468b1fb87140213aa3e
Jeonghee Kim <jeongheekim219@gmail.com>
2022-08-23 22:06:38 +0900

    2022-08-23 toDoContent has been trimmed on div.

--- src/main/resources/static/js/calendarDataShow.js
+++ src/main/resources/static/js/calendarDataShow.js
@@ -7,45 +7,12 @@ function addTodoOnCalendar(result){
 
     for (var i = 0; i < result.length; i++){
         var $div = document.createElement('div');
-        $div.textContent = result[i].toDoContent;
+        // DB의 to_do_content 데이터를 trim, 공백삭제
+        $div.textContent = result[i].toDoContent.replaceAll(" ", "").trim();
         var planDate = new Date(result[i].planDate);
         var tdId = "#" + setDateId(planDate, planDate.getDate());
-        console.log(tdId);
-        $(tdId).css("border", "1px solid red");
         $(tdId).append($div);
-
     }
-
-
-//        var table4Todo = document.getElementById("toDoListsTable");
-//
-//        var indexRaw = table4Todo.insertRow();
-//        var cell1 = indexRaw.insertCell(0);
-//        var cell2 = indexRaw.insertCell(1);
-//        var cell3 = indexRaw.insertCell(2);
-//        var cell4 = indexRaw.insertCell(3);
-//        var cell5 = indexRaw.insertCell(4);
-//
-//        cell1.innerText = ' ';
-//        cell2.innerText = 'content';
-//        cell3.innerText = 'state';
-//        cell4.innerText = ' ';
-//        cell5.innerText = ' ';
-//
-//        for(var obj of resultTodo){
-//            var newRaw = table4Todo.insertRow();
-//            var color = newRaw.insertCell(0);
-//            var content = newRaw.insertCell(1);
-//            var state = newRaw.insertCell(2);
-//            var deleteBtn = newRaw.insertCell(3);
-//
-//            var values = Object.values(obj);
-//
-//            color.innerText = "●";
-//
-//            content.innerText = values[2];
-//            state.innerText = values[4];
-//            deleteBtn.innerText = "delete";
 
 
 }
