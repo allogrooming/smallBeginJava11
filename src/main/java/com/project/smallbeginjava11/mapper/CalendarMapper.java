2e34149e50a3cb28455baa5a6b89426391a468fe
Jeonghee Kim <jeongheekim219@gmail.com>
2022-08-23 15:59:53 +0900

    2022-08-23 Downsized.

--- src/main/java/com/project/smallbeginjava11/mapper/CalendarMapper.java
+++ src/main/java/com/project/smallbeginjava11/mapper/CalendarMapper.java
@@ -1,7 +1,6 @@
 package com.project.smallbeginjava11.mapper;
 
 import com.project.smallbeginjava11.DTO.Calendar;
-import com.project.smallbeginjava11.DTO.Initiative;
 import com.project.smallbeginjava11.DTO.Todo;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
--- src/main/java/com/project/smallbeginjava11/service/MonthListService.java
+++
@@ -1,9 +0,0 @@
-package com.project.smallbeginjava11.service;
-
-import java.util.List;
-import java.util.Map;
-
-public interface MonthListService {
-    void insertMonthList(Map<String, Object> map);
-    List<String> selectMonthList(int monthListCode);
-}
