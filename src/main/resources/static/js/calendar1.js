eb4d75fe76fca84852fc10b9958e5b531fca5dca
allogrooming <gr2nteafrappuccino@gmail.com>
2022-08-24 20:57:48 +0900

    edit 진행중

--- src/main/resources/static/js/calendar1.js
+++ src/main/resources/static/js/calendar1.js
@@ -577,42 +577,20 @@ function addTodo(result){
     }
 }
 
-function editTodo(result){
-    var resultTodo = JSON.parse(result);
-
-    if(resultTodo.length > 0){
-        var table4Todo = document.getElementById("toDoListsTable");
-
-        for(var obj of resultTodo){
-            var values = Object.values(obj);
-
-            var newRaw = table4Todo.insertRow();
-            var color = newRaw.insertCell(0);
-            var content = newRaw.insertCell(1);
-            var state = newRaw.insertCell(2);
-            var deleteBtn = newRaw.insertCell(3);
-            var editBtn = newRaw.insertCell(4);
-
-            color.innerText = "●";
-            color.id = values[5];
-            deleteBtn.id = values[0];
-            color.classList.add("color");
-            content.classList.add("content");
-            deleteBtn.classList.add("delete");
-            editBtn.classList.add("edit");
-            editBtn.classList.add(i);
-
-            content.innerText = values[2];
-            state.innerText = values[4];
-            deleteBtn.innerText = "delete";
-            editBtn.innerText = "edit";
-
-            var tester = document.getElementById(values[5]);
-            var colorTodo = "#" + values[5];
-            $(tester).css("color", values[5]);
-            i++;
-        }
-    }
+function editTodo(sendColor, sendContent, clickedDate, toDoCode){
+     $.ajax({
+             url : "/editToDo",
+             type : "post",
+             contentType: 'application/x-www-form-urlencoded; charset=utf-8',
+             dataType : "text",
+             data : {"toDoCode" : toDoCode, "toDoContent" : toDoContent, "toDoColor" : toDoColor},
+             success : function(result){
+                 readToDo(clickedDate);
+             },
+             error : function(err){
+                 console.log(err+"에러발생");
+             }
+      });
 }
 
 function readToDo(clickedDate){
