0fcca298b4ea6b44a028c8abe5786d182a6e752a
Jeonghee Kim <jeongheekim219@gmail.com>
2022-09-01 19:30:34 +0900

    2022-09-01 mapper scan

--- src/main/java/com/project/smallbeginjava11/SmallBeginJava11Application.java
+++ src/main/java/com/project/smallbeginjava11/SmallBeginJava11Application.java
@@ -1,9 +1,11 @@
 package com.project.smallbeginjava11;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(value = "com.project.smallbeginjava11.mapper")
 public class SmallBeginJava11Application {
 
     public static void main(String[] args) {
