195f16a008d6c07fad83a93bf4ddd06e59b35d7a
allogrooming <gr2nteafrappuccino@gmail.com>
2022-09-03 21:56:04 +0900

    thymeleaf try #3

---
+++ src/main/java/com/project/smallbeginjava11/DTO/Login.java
@@ -0,0 +1,16 @@
+package com.project.smallbeginjava11.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Login {
+    private int memberCode;
+    private String memberId;
+    private String password;
+}
