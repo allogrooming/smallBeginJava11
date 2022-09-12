58a82a5cf6b12b9b40c36580c59016f5b00717bd
Jeonghee Kim <jeongheekim219@gmail.com>
2022-09-12 15:48:56 +0900

    2022-09-12 DTO class name has been renamed.

--- src/main/java/com/project/smallbeginjava11/mapper/CalendarMapper.java
+++ src/main/java/com/project/smallbeginjava11/mapper/CalendarMapper.java
@@ -1,6 +1,5 @@
 package com.project.smallbeginjava11.mapper;
 
-import com.project.smallbeginjava11.DTO.Calendar;
 import com.project.smallbeginjava11.DTO.Todo;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
@@ -13,7 +12,6 @@ import java.util.Map;
 public interface CalendarMapper {
     void inputTodoList(Map<String, Object> params);
     List<Todo> getTodoList(Map<String, Object> params);
-    List<Calendar> selectIniAndObList(Map<String, Object> map);
     List<Todo> getTodoListInMonth(Map<String, Object> params);
     void updateToDoState(Map<String, Object> params);
     void toDoDelete(Map<String, String> params);
